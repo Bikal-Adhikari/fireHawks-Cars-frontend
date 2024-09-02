03a683696843993a091a06fe747e5bf5b8ad6f3e
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-09-02 14:04:43 +1000

    User authentication using firebase

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { UsersService } from 'src/app/services/users.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  user$ = this.usersService.currentUserProfile$;
+
+  constructor(
+    private usersService: UsersService,
+    public authService: AuthService
+  ) {}
+
+  ngOnInit(): void {}
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyCLW8y44Ua1h7LE5avIgI23W10mR7xeoUk',
+    authDomain: 'firehawk-cars.firebaseapp.com',
+    projectId: 'firehawk-cars',
+    storageBucket: 'firehawk-cars.appspot.com',
+    messagingSenderId: '551656659070',
+    appId: '1:551656659070:web:f9739dcc4c2787b279a02f',
+  },
+  production: false,
+};
