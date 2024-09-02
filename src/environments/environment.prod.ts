252383eecc9437125a420525737e4b3ba2f23596
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-09-02 17:53:33 +1000

    firebase config added for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
-    projectId: 'angular-sign-up',
-    appId: '1:916691174045:web:677a900ca493c7d0e9ca74',
-    storageBucket: 'angular-sign-up.appspot.com',
-    apiKey: 'AIzaSyAaS6ztStcHbGSznGNBchaa_ZN5B02Zk6c',
-    authDomain: 'angular-sign-up.firebaseapp.com',
-    messagingSenderId: '916691174045',
+    apiKey: 'AIzaSyCLW8y44Ua1h7LE5avIgI23W10mR7xeoUk',
+    authDomain: 'firehawk-cars.firebaseapp.com',
+    projectId: 'firehawk-cars',
+    storageBucket: 'firehawk-cars.appspot.com',
+    messagingSenderId: '551656659070',
+    appId: '1:551656659070:web:f9739dcc4c2787b279a02f',
   },
-  production: true
+  production: true,
 };
